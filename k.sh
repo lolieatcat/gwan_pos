cecf60d35612b294a73bc18fbbdf90640e5e1105
Ubuntu <ubuntu@ip-172-31-6-211.us-west-2.compute.internal>
2019-05-08 06:15:28 +0000

    add start script

---
+++ k.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./gwan_ktest --plutodev --nodiscover --datadir ./ktest --port 50101 --mine --etherbase "cf696d8EEA08a311780fB89B20d4F0895198a489" --unlock "cf696d8EEA08a311780fB89B20d4F0895198a489" --password ~/pw.txt
+
+
